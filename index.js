a3f521d6416ccca4f15bf150ad5b249bde236ad7
Brett B <65505937+BreadBooks@users.noreply.github.com>
2021-01-11 19:22:22 -0600

    minor edit

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ async function init() {
   }
 }
 
+var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+var toastList = toastElList.map(function (toastEl) {
+  return new bootstrap.Toast(toastEl, option)
+})
+
+function myToast() {
+  toast.show()
+}
