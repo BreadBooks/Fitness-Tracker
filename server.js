c84d6faf10dc72ca09e5c400e4230da6b835dce0
Brett B <65505937+BreadBooks@users.noreply.github.com>
2021-01-16 11:24:12 -0600

    maybe fix

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://beep:beep@cluster0.tuo5m.mongodb.net/workout?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://beep:beep@cluster0.tuo5m.mongodb.net/workout?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 const app = express();
 
 app.use(logger("dev"));
