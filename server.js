70e260fa17d9ca01c85e5a3aa89c563c7a60929c
Brett B <65505937+BreadBooks@users.noreply.github.com>
2021-01-16 14:55:28 -0600

    deploying

--- server.js
+++ server.js
@@ -3,14 +3,7 @@ const mongoose = require("mongoose");
 const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://beep:beep@cluster0.tuo5m.mongodb.net/workout?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
+
 const app = express();
 
 app.use(logger("dev"));
@@ -20,12 +13,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/fitness-tracker',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
